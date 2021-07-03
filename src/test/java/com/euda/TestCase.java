d5ca5cbaccae9b492cf5474aea1ba89bc577657c
brijeshyadav <brijesh79@hotmail.com>
2021-07-04 00:26:18 +0530

    testcase

---
+++ src/test/java/com/euda/TestCase.java
@@ -0,0 +1,14 @@
+package com.euda;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
